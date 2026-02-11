ac9835b88ffe5949959959c1b43422d4750560d2
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 14:36:46 +0000

    comparsion

---
+++ 01_basics/05_comparsion.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<=1);
+// console.log("2">1)
+// console.log("02">1);
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+// === stricty checked (value and datatypes)
+console.log(" 2"===2);
+
+
+
