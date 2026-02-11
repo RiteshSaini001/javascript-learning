4e293af4ac4060561fbfebeb4f897c92c2da7349
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 14:59:39 +0000

    datatypes_summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// #primitive
+// 7 types: String,Number,Boolean ,Null, undefined,Symbol,BigInt
+
+//example
+const score =100
+const scoreValue=100.3
+
+const isLoggedIn =false
+const outSideTemp=null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+console.table([id,anotherId])
+console.log( id===anotherId);
+
+//Reference type / Non primitive
+//Array,Objects,Functions
+const heros =["shaktiman","ben 10"," ironman"]
+let myObj={
+    name:"ritesh",
+    age:21,
+}
+const myfunction=function(){
+     console.log("hello ritik");
+}
