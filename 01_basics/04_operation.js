6a4d9729bf5fa42a40dae291ce9a40bcc9049acf
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 14:22:39 +0000

    operation

--- 01_basics/04_operation.js
+++ 01_basics/04_operation.js
@@ -12,10 +12,18 @@ let str2 =" saini"
 let str3 =str1 + str2
 console.log(str3);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+"2");
-console.log("1"+2+2);
-console.log(1+2+"2");
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+"2");
+//console.log("1"+2+2);
+//console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter =100
+gameCounter++
+console.log(gameCounter);
+
 
 
