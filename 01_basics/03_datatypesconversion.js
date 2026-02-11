d3c97480417d96e5b3c3f8eafa3db7a4960081ec
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 12:49:56 +0000

    datatypes of conversion

---
+++ 01_basics/03_datatypesconversion.js
@@ -0,0 +1,43 @@
+let score ="33abc"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//null
+let score1 =null
+
+console.log(typeof score1)
+console.log(typeof (score1))
+
+let valueInNumber1=Number(score1)
+console.log(typeof valueInNumber1)
+console.log(valueInNumber1)
+
+//undefined
+let score2 =undefined
+
+console.log(typeof score2)
+console.log(typeof (score2))
+
+let valueInNumber2=Number(score2)
+console.log(typeof valueInNumber2)
+console.log(valueInNumber2)
+//boolean 
+let score3 ="ritik"
+
+console.log(typeof score3)
+
+
+let valueInNumber3=Boolean(score3)
+console.log(typeof valueInNumber3)
+console.log(valueInNumber3)
+//"33" =>33
+//"33abc" =>NaN
+//bool  true false=> 1/0
+//null =>0
+//undefined => NaN
+//boolean 1 =>true ,0=> false ,"" =>false ,"ritik" => true;
