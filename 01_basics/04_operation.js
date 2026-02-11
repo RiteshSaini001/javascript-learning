046941fa82a60e64c4666cb12494efaf164fc9ee
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 14:13:40 +0000

    operation

---
+++ 01_basics/04_operation.js
@@ -0,0 +1,21 @@
+let value =3
+let Negvalue =-value
+//console.log(Negvalue);
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**4);
+//console.log(2/2);
+//console.log(2%2);
+let str1 ="ritik"
+let str2 =" saini"
+let str3 =str1 + str2
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
