22a8573288e022a70a455444586fac4ea6cf2eb9
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 12:18:59 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=2309235
+let accountEmail="hiibsiovsn"
+var accountPassword="323248346"
+accountCity ="jaipur"
+
+//accountId=2 //do not change
+console.log(accountId)
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity);
+console.table([accountId,accountEmail,accountPassword,accountCity])
+accountEmail="sdklbnaalkn"
+accountPassword="1249024"
+accountCity="udaipur"
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" ;//treat all js code as newer version
+
+//alert(3+3) //we are using not js not browser
+console.log(3
+    +3)//code readability should be high
+
+console.log("ritik")   
+let name="name"
+let age =18
+let isLoggedIn= false
+//number => 2 to power53
+//big int
+//string => in double qoutes" ""
+// boolean =>true /false
+//null =>standalone value
+//undefined  =>
+//symbol =.unique
+
+//object
+
+console.log(typeof age);
+
+
+
