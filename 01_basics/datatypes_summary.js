e659fd1f1cc6be26da864dea48223592696a1aa0
RiteshSaini001 <r57565992@gmail.com>
2026-02-11 17:10:29 +0000

    datatypes_summary

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -24,3 +24,13 @@ let myObj={
 const myfunction=function(){
      console.log("hello ritik");
 }
+//// ++++++++++++++++++++++++++++++++
+
+//stack memory(primitive),heap memory(Non Primitive)
+let myYoutubeName="ritiksaini"
+let anotherName=myYoutubeName
+anotherName="ritesh"
+
+console.log(myYoutubeName);
+console.log(anotherName);
+
